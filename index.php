e8c4ccd474e9c694f79d1c0bb0671f35be02754c
derfoh <fredrick.p@outlook.com>
2016-09-15 19:16:43 +0000

    Added bootstrap

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <html>
  <head>
   <title>PHP Test</title>
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css"> 
  </head>
+ 
  <body>
  <?php echo '<p>Hello World</p>'; ?> 
+ 
  </body>
 </html>
