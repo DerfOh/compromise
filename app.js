6d3132dd2c9423a6c28b180024e5ff63efa5c5d9
DerfOh <fredrick.p@outlook.com>
2016-09-20 20:56:51 -0400

    Started simple js app to start figuring out jasmine

---
+++ app.js
@@ -0,0 +1,11 @@
+// example javascript test
+
+function add (x, y){
+  return x+y;
+}
+
+add(2,3); //5
+
+// 1. expect 2+3 =5
+// 2. expect error if non numbers are used
+// 3. expect 0.1 + 0.2 = 0.3
