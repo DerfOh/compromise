0eb351054354707c6e918c5416915d0cd7429e78
DerfOh <fredrick.p@outlook.com>
2016-10-25 03:55:04 -0400

    Add SQL

---
+++ initialize-tables.sql
@@ -0,0 +1,79 @@
+/* Tables to initialize the sql database  */
+CREATE TABLE Users
+(
+EmailAddress varchar(255),
+FirstName varchar(255),
+LastName varchar(255),
+Nickname varchar(255),
+Password varchar(255),
+PRIMARY KEY(EmailAddress)
+);
+
+CREATE TABLE Groups
+(
+GroupId int AUTO_INCREMENT,
+GroupName varchar(255),
+PRIMARY KEY (GroupId)
+);
+
+CREATE TABLE Rewards
+(
+RewardId int AUTO_INCREMENT,
+GroupId int,
+RewardName varchar(255),
+PointCost int,
+RewardDescription varchar(255),
+PRIMARY KEY (RewardId),
+CONSTRAINT fk_GroupReward FOREIGN KEY (GroupId)
+REFERENCES Groups (GroupId)
+);
+
+CREATE TABLE RewardRequests
+(
+RequestId int AUTO_INCREMENT,
+RewardId int,
+FulfillmentStatus varchar(255),
+PRIMARY KEY (RequestId),
+CONSTRAINT fk_RewardRequest FOREIGN KEY (RewardId)
+REFERENCES Rewards (RewardId)
+);
+
+CREATE TABLE Tasks
+(
+TaskId int AUTO_INCREMENT,
+GroupId int,
+TaskName varchar(255),
+TaskDescription varchar(255),
+DateDue DATETIME,
+ApprovalStatus varchar(255),
+CompletionStatus varchar(255),
+PointValue int,
+PRIMARY KEY (TaskId),
+CONSTRAINT fk_GroupTask FOREIGN KEY (GroupId)
+REFERENCES Groups(GroupId)
+);
+
+CREATE TABLE TaskLeaders
+(
+TaskLeaderId int AUTO_INCREMENT,
+EmailAddress varchar(255),
+GroupId int,
+PRIMARY KEY (TaskLeaderId),
+CONSTRAINT fk_GroupLeader FOREIGN KEY (GroupId)
+REFERENCES Groups (GroupId),
+CONSTRAINT fk_GroupUser FOREIGN KEY (EmailAddress)
+REFERENCES Users (EmailAddress)
+);
+
+CREATE TABLE Points
+(
+PointId int AUTO_INCREMENT,
+TotalPoints int DEFAULT 0,
+EmailAddress varchar(255),
+GroupId int,
+PRIMARY KEY (PointId),
+CONSTRAINT fk_GroupPoint FOREIGN KEY (GroupId)
+REFERENCES Groups (GroupId),
+CONSTRAINT fk_UserPoint FOREIGN KEY (EmailAddress)
+REFERENCES Users (EmailAddress)
+);
