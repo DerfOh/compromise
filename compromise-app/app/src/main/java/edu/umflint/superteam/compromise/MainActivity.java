4235fc4a64fba1bf75cfe91729cd3d1686bb8959
Nicholas Sammut <njsammut95@gmail.com>
2016-11-11 12:30:57 -0500

    Implement Login

--- compromise-app/app/src/main/java/edu/umflint/superteam/compromise/MainActivity.java
+++ compromise-app/app/src/main/java/edu/umflint/superteam/compromise/MainActivity.java
@@ -1,7 +1,9 @@
 package edu.umflint.superteam.compromise;
 
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
@@ -14,6 +16,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
 import com.aurelhubert.ahbottomnavigation.AHBottomNavigationAdapter;
@@ -38,6 +41,9 @@ public class MainActivity extends AppCompatActivity
         toggle.syncState();
 
 
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String name = preferences.getString("EmailAddress", "");
+        Toast.makeText(getApplication().getBaseContext(), name, Toast.LENGTH_SHORT).show();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
