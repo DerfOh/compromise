9a757f1e5c18c32b834ba82a61fbe0011efdc898
DerfOh <fredrick.p@outlook.com>
2016-11-12 22:34:33 -0500

    Update readme
    Update README.md

--- api/users.go
+++ api/users.go
@@ -142,7 +142,7 @@ func UserAPIHandler(response http.ResponseWriter, request *http.Request) {
 	// Clean up JSON before returning
 	// Send the text diagnostics to the client.
 	//fmt.Fprintf(response, "%v", CleanJSON(string(json)))
-	fmt.Fprintf(response, "%v", CleanJSON(string(json))
+	fmt.Fprintf(response, "%v", CleanJSON(string(json)))
 
 	db.Close()
 }
