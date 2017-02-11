7a7ec9bff2399d581429387b6dcdd24ea422c662
Fredrick Paulin <fredrick.p@outlook.com>
2017-02-11 15:43:08 -0500

    Update Groups sample data insert source code

--- database/GroupsSampleData.sql
+++ database/GroupsSampleData.sql
@@ -1,7 +1,8 @@
-INSERT INTO Groups
-    (`groupid`, `groupname`)
-VALUES    
-    (1,'Family')
-    (2,'Dairy Queen')
-    (3,'383 Project')
-    (4,'android Project')
+INSERT INTO compromise.Groups
+	(`groupid`, `groupname`)
+VALUES
+	(1, 'Family'),
+	(2, 'Dairy Queen'),
+	(3, '383 Project'),
+	(4, 'Android Project')
+;
