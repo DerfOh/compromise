363a945cf5d430e246921058985760e2d6e9ddfa
Fredrick Paulin <fredrick.p@outlook.com>
2017-02-11 16:31:38 -0500

    Create insert statements for sample Points data

---
+++ database/PointsSampleData.sql
@@ -0,0 +1,17 @@
+INSERT INTO compromise.Points
+  (`PointId`,`TotalPoints`,`EmailAddress`,`GroupId`)
+VALUES
+  (1,2500,'nsammut@umflint.edu',4),
+  (2,225,'fpaulin@umflint.edu',1),
+  (3,1406,'fpaulin@umflint.edu',3),
+  (4,3486,'tmckeoun@umflint.edu',3),
+  (5,21,'tmckeoun@umflint.edu',2),
+  (6,1732,'tmckeoun@umflint.edu',4),
+  (7,2494,'samerab@umflint.edu',2),
+  (8,1899,'randile@umflint.edu',4),
+  (9,2788,'nsammut@umflint.edu',1),
+  (10,432,'randile@umflint.edu',1),
+  (11,6546,'randile@umflint.edu',2),
+  (12,654,'samerab@umflint.edu',4),
+  (13,6566,'samerab@umflint.edu',1)
+;
