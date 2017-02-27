bfc5b508daf2d2348fae9fe2f8ab2c870d7d33d3
Fredrick Paulin <fredrick.p@outlook.com>
2017-02-27 13:44:33 -0500

    Write test for password hashing

---
+++ api/passwords_test.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func ExampleHash() {
+	// s := CleanJSON("}\"")
+	// fmt.Println(s)
+
+	// s = CleanJSON("\"{")
+	// fmt.Println(s)
+
+	// s = CleanJSON("\\\"")
+	// fmt.Println(s)
+
+	// // Output: }
+	// // {
+	// // "
+
+	password := "secret"
+	hash, _ := HashPassword(password) // ignore error for the sake of simplicity
+
+	fmt.Println("Password:", password)
+	//fmt.Println("Hash:    ", hash)
+
+	match := CheckPasswordHash(password, hash)
+	fmt.Println("Match:   ", match)
+
+	// Output: Password: secret
+	// Match:    tru
+
+}
